db244a977cbb8d1731c2af063f8f5176f59591fb
B. P. Hon <baijiephoneycutt@gmail.com>
2024-07-07 14:42:57 -0400

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="py-8">
-    <h1 class="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-    <p>This is the home page of your application.</p>
+  <div class="py-10">
+    <h1 class="text-4xl font-bold mb-4">Welcome to Github Workflow Demo</h1>
+    <p>This is the home page of Github Workflow Demo.</p>
   </div>
 </template>
